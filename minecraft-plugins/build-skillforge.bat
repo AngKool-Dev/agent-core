b2d056fcd1006e60564fefc51fb7d1fb62a2ab8e
John Paul Asonio <asoniojohnpaul@gmail.com>
2026-08-30 15:05:11 +0800

    chore: remove runtime files from git and update .gitignore

--- minecraft-plugins/build-skillforge.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d D:\agent-core\minecraft-plugins
-call D:\agent-core\maven\apache-maven-3.9.6\bin\mvn.cmd clean install -pl skillforge -am -Dmaven.compiler.proc=none -Dmaven.compiler.argument=-XDuseUnsharedTable=false
-echo.
-echo === EXIT CODE: %ERRORLEVEL% ===
